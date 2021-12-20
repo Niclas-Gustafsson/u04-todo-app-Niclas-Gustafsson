bf0999285cae05bcc8464b17064e51bed3e6cf2f
Niclas-Gustafsson <niclas.gustafsson@chasacademy.se>
2021-12-20 10:40:33 +0100

    Add index html markup

--- app/public/index.php
+++ app/public/index.php
@@ -14,6 +14,29 @@
     <title>Thapp</title>
 </head>
 <body>
+    <main class="main">
+        <section class="container">
+            <nav class="nav">
+                <div class="logo-wrapper nav-left">
+                    <img src="" alt="">
+                </div>
+                <div class="navigation nav-right">
+                    <ul class="nav-items">
+                        <li class="nav-item"><a class="nav-item_links active" href="">Thapp</a></li>
+                        <li class="nav-item"><a class="nav-item_links" href="">Login</a></li>
+                        <li class="nav-item"><a class="nav-item_links" href="">Signup</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </section>
+        <section class="main-dashboard">
+            <h1 class="heading">Thapp</h1>
+            <h2 class="sub-heading">Thapp is a simplified note-taking app to get your days running smoothly.</h2>
+
+            <p class="cta-text">Sign up now and boost your productivity</p>
+            <a href="signup.php" class="cta">Sign up!</a>
+        </section>
+    </main>
 
 </body>
 </html>
